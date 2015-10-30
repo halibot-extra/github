775a5a4fa2959ed4c8941a6d6990092d9696493a
Chris Harding <wablahflababablah@gmail.com>
2015-10-30 17:30:55 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,57 @@
+Halibot Github Module
+=====================
+
+Reports changes to github repositories.
+
+How to use
+----------
+
+This module will spin up a server that will listen for github hook events. You
+will need to enable a hook in your github repository or organization and have it
+point to the server you are running this module on. The port number of the
+server can be changed with the `port` instance config field. It is recommended
+that you use a secret key shared between github and this module. This key can be
+set with the `secret` field.
+
+You will need to tell this module where to send the reports, which is done with
+the `context` instance config field. You will also need to tell this module what
+events and actions to listen for. This is done with the `events` field which is
+a map of all events to listen for to a list of actions to listen for.
+
+Example config
+--------------
+
+```json
+{
+  ...
+
+
+  "module-instances": {
+    ...
+
+    "github-example": {
+      "of": "github",
+      "secret": "sshhhh",
+      "context": {
+        "agent": "irc0",
+        "protocol": "irc",
+        "whom": "##example"
+      },
+      "events": {
+        "issues": [
+          "opened",
+          "reopened",
+          "closed"
+        ],
+        "pull_request": [
+          "opened",
+          "reopened",
+          "closed"
+        ]
+      }
+    }
+
+    ...
+  }
+}
+```
---
+++ github.py
@@ -0,0 +1,118 @@
+
+import json
+import hmac, hashlib
+import threading
+import http, http.server
+from halibot import HalModule
+
+def make_issues_report(event, payload):
+	title = payload['issue']['title']
+	user = payload['issue']['user']['login']
+	repo = payload['repository']['full_name']
+
+	if payload['action'] == 'opened':
+		return 'New issue "{}" opened by {} in {}.'.format(title, user, repo)
+
+	if payload['action'] == 'reopened':
+		return 'Issue "{}" reopened by {} in {}.'.format(title, user, repo)
+
+	if payload['action'] == 'closed':
+		return 'Issue "{}" closed by {} in {}.'.format(title, user, repo)
+
+	return None
+
+def make_pr_report(event, payload):
+	title = payload['pull_request']['title']
+	user = payload['pull_request']['user']['login']
+	repo = payload['repository']['full_name']
+
+	if payload['action'] == 'opened':
+		return 'Pull request "{}" opened by {} in {}.'.format(title, user, repo)
+
+	if payload['action'] == 'reopened':
+		return 'Pull request "{}" reopened by {} in {}.'.format(title, user, repo)
+
+	if payload['action'] == 'closed':
+		return 'Pull request "{}" closed by {} in {}.'.format(title, user, repo)
+
+	return None
+
+def make_report(event, payload):
+	fun = {
+		'issues': make_issues_report,
+		'pull_request': make_pr_report,
+	}.get(event, None)
+	
+	if fun == None:
+		return None
+
+	return fun(event, payload)
+
+class GithubHookHandler(http.server.BaseHTTPRequestHandler):
+
+	def do_POST(self):
+		module = self.server.module
+		config = module.config
+
+		# filter out non-github events
+		if not 'X-Github-Event' in self.headers:
+			# Ignore and timeout
+			module.log.info('Received something that is not a github event, ignoring.')
+			return
+	
+		length = int(self.headers['Content-Length'])
+		data = self.rfile.read(length)
+
+		# Do hmac verification if enabled
+		# Enabling recommend so people don't send random garbage to the endpoint
+		if 'secret' in config:
+			h = hmac.new(config['secret'].encode(), msg=data, digestmod=hashlib.sha1)
+			expect = 'sha1=' + h.hexdigest()
+
+			if expect != self.headers['X-Hub-Signature']:
+				module.log.warning('HMAC signature mismatch!')
+				return
+
+		# TODO read charset from header
+		payload = json.loads(data.decode('utf-8'))
+
+		event = self.headers['X-Github-Event']
+		action = payload.get('action', None)
+
+		module.log.debug('Received {} {} event.'.format(event, action))
+		
+		# ignore events we didn't ask for
+		events = module.events
+		if event in events and action in events[event]:
+			report = make_report(event, payload)
+
+			# Ignore events we don't know how to handle
+			if report != None:
+				msg = {
+					'body': report,
+					'author': 'halibot',
+					'context': config['context']
+				}
+				module.log.debug('Reporting event to ' + config['context']['whom'])
+				module.send(msg)
+			else:
+				module.log.warning('Could not form report for "{} {}"'.format(event, action))
+
+		self.send_response(204)
+		self.end_headers()
+
+class GithubModule(HalModule):
+
+	def init(self):
+		self.events = self.config.get('events', {})
+
+		addr = ('', self.config.get('port', 9000))
+		self.server = http.server.HTTPServer(addr, GithubHookHandler)
+		self.server.module = self
+		self.thread = threading.Thread(target=self.server.serve_forever)
+		self.thread.start()
+
+	def shutdown(self):
+		self.server.shutdown()
+		self.thread.join()
+
